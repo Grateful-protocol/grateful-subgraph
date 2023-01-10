80add965015aa32d1920bc5f58674e4740024d50
agusduha <agusnduha@gmail.com>
2023-01-10 15:01:21 -0300

    add funds deposited mapping

--- src/funds.ts
+++ src/funds.ts
@@ -1,1 +1,21 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { FundsDeposited } from "../generated/FundsModule/FundsModule";
+import { Profile, Balance } from "../generated/schema";
+
+export function handleFundsDeposited(event: FundsDeposited): void {
+  const profileId = event.params.profileId;
+  const vaultId = event.params.vaultId;
+  const amount = event.params.shares;
+
+  const balanceId = profileId.concat(vaultId);
+  let balance = Balance.load(balanceId);
+  if (balance == null) {
+    balance = new Balance(balanceId);
+    balance.vault = vaultId;
+    balance.profile = profileId;
+  }
+
+  balance.amount = balance.amount.plus(amount);
+  balance.lastUpdate = event.block.timestamp;
+
+  balance.save();
+}
