f981804c35ac70b4c2b9836e85c26f73dcbff612
agusduha <agusnduha@gmail.com>
2023-01-16 18:08:44 -0300

    add subscriptions mappings

--- src/subscriptions.ts
+++ src/subscriptions.ts
@@ -1,1 +1,42 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  GratefulSubscription as Subscription,
+  Balance,
+} from "../generated/schema";
+import {
+  SubscriptionFinished,
+  SubscriptionStarted,
+} from "../generated/SubscriptionsModule/SubscriptionsModule";
+
+export function handleSubscriptionStarted(event: SubscriptionStarted): void {
+  const subscriptionId = event.params.subscriptionId.toString();
+
+  let subscription = Subscription.load(subscriptionId);
+
+  if (subscription == null) {
+    subscription = new Subscription(subscriptionId);
+    subscription.giver = event.params.giverId;
+    subscription.creator = event.params.creatorId;
+  }
+
+  subscription.vault = event.params.vaultId;
+  subscription.rate = event.params.rate;
+  subscription.feeRate = event.params.feeRate;
+  subscription.lastUpdate = event.block.timestamp;
+
+  subscription.save();
+}
+
+export function handleSubscriptionFinished(event: SubscriptionFinished): void {
+  const subscriptionId = event.params.subscriptionId.toString();
+
+  let subscription = Subscription.load(subscriptionId);
+
+  if (subscription) {
+    subscription.vault = null;
+    subscription.rate = null;
+    subscription.feeRate = null;
+    subscription.lastUpdate = event.block.timestamp;
+
+    subscription.save();
+  }
+}
