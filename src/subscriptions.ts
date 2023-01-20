cec61ee210980a4354b0c2ab0c3a942cca8e7aef
agusduha <agusnduha@gmail.com>
2023-01-20 15:28:55 -0300

    refactor subscriptions mappings

--- src/subscriptions.ts
+++ src/subscriptions.ts
@@ -1,33 +1,53 @@
-import {
-  GratefulSubscription as Subscription,
-  Balance,
-} from "../generated/schema";
+import { GratefulSubscription as Subscription } from "../generated/schema";
 import {
   SubscriptionFinished,
   SubscriptionStarted,
 } from "../generated/SubscriptionsModule/SubscriptionsModule";
+import { handleBalanceFlowChange } from "./balances";
+import { FeesModule } from "../generated/SubscriptionsModule/FeesModule";
 
 export function handleSubscriptionStarted(event: SubscriptionStarted): void {
   const subscriptionId = event.params.subscriptionId.toString();
+  const giverId = event.params.giverId;
+  const creatorId = event.params.creatorId;
+  const vaultId = event.params.vaultId;
+  const rate = event.params.rate;
+  const feeRate = event.params.feeRate;
 
   let subscription = Subscription.load(subscriptionId);
 
   if (subscription == null) {
     subscription = new Subscription(subscriptionId);
-    subscription.giver = event.params.giverId;
-    subscription.creator = event.params.creatorId;
+    subscription.giver = giverId;
+    subscription.creator = creatorId;
   }
 
-  subscription.vault = event.params.vaultId;
-  subscription.rate = event.params.rate;
-  subscription.feeRate = event.params.feeRate;
+  subscription.vault = vaultId;
+  subscription.rate = rate;
+  subscription.feeRate = feeRate;
   subscription.lastUpdate = event.block.timestamp;
 
   subscription.save();
+
+  // Handle giver balance
+  const totalFlow = rate.plus(feeRate).neg();
+  handleBalanceFlowChange(event, giverId, vaultId, totalFlow);
+
+  // Handle creator balance
+  handleBalanceFlowChange(event, creatorId, vaultId, rate);
+
+  // Handle treasury balance
+  const treasuryId = FeesModule.bind(event.address).getFeeTreasuryId();
+  handleBalanceFlowChange(event, treasuryId, vaultId, feeRate);
 }
 
 export function handleSubscriptionFinished(event: SubscriptionFinished): void {
   const subscriptionId = event.params.subscriptionId.toString();
+  const giverId = event.params.giverId;
+  const creatorId = event.params.creatorId;
+  const vaultId = event.params.vaultId;
+  const rate = event.params.rate;
+  const feeRate = event.params.feeRate;
 
   let subscription = Subscription.load(subscriptionId);
 
@@ -38,5 +58,16 @@ export function handleSubscriptionFinished(event: SubscriptionFinished): void {
     subscription.lastUpdate = event.block.timestamp;
 
     subscription.save();
+
+    // Handle giver balance
+    const totalFlow = rate.plus(feeRate);
+    handleBalanceFlowChange(event, giverId, vaultId, totalFlow);
+
+    // Handle creator balance
+    handleBalanceFlowChange(event, creatorId, vaultId, rate.neg());
+
+    // Handle treasury balance
+    const treasuryId = FeesModule.bind(event.address).getFeeTreasuryId();
+    handleBalanceFlowChange(event, treasuryId, vaultId, feeRate.neg());
   }
 }
