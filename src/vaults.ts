f3bdebb55a271e4f77fa95f8f5a7114be4aabbb3
agusduha <agusnduha@gmail.com>
2023-01-20 10:26:47 -0300

    add fields to vault

--- src/vaults.ts
+++ src/vaults.ts
@@ -2,6 +2,7 @@ import { VaultAdded } from "../generated/VaultsModule/VaultsModule";
 import { Vault } from "../generated/schema";
 
 export function handleVaultAdded(event: VaultAdded): void {
-  let vault = new Vault(event.params.id);
+  const vault = new Vault(event.params.id);
+  vault.address = event.params.impl;
   vault.save();
 }
