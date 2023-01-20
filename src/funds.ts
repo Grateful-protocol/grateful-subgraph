ba6ca611a4e5252447b29ae34bea758bde3b8315
agusduha <agusnduha@gmail.com>
2023-01-20 15:28:38 -0300

    refactor funds mappings

--- src/funds.ts
+++ src/funds.ts
@@ -2,30 +2,18 @@ import {
   FundsDeposited,
   FundsWithdrawn,
 } from "../generated/FundsModule/FundsModule";
-import { getBalance } from "./balances";
+import { handleBalanceAmountChange } from "./balances";
 
 export function handleFundsDeposited(event: FundsDeposited): void {
   const profileId = event.params.profileId;
   const vaultId = event.params.vaultId;
   const amount = event.params.shares;
-
-  const balance = getBalance(profileId, vaultId);
-
-  balance.amount = balance.amount.plus(amount);
-  balance.lastUpdate = event.block.timestamp;
-
-  balance.save();
+  handleBalanceAmountChange(event, profileId, vaultId, amount);
 }
 
 export function handleFundsWithdrawn(event: FundsWithdrawn): void {
   const profileId = event.params.profileId;
   const vaultId = event.params.vaultId;
-  const amount = event.params.shares;
-
-  const balance = getBalance(profileId, vaultId);
-
-  balance.amount = balance.amount.minus(amount);
-  balance.lastUpdate = event.block.timestamp;
-
-  balance.save();
+  const amount = event.params.shares.neg();
+  handleBalanceAmountChange(event, profileId, vaultId, amount);
 }
