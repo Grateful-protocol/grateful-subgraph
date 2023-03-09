5ce2a05ef54a5fefe5c891f64c29ae1941d0ccae
agusduha <agusnduha@gmail.com>
2023-03-09 16:47:18 -0300

    update vaults event handlers

--- src/vaults.ts
+++ src/vaults.ts
@@ -1,8 +1,41 @@
-import { VaultAdded } from "../generated/VaultsModule/VaultsModule";
+import {
+  MaxRateChanged,
+  MinRateChanged,
+  VaultAdded,
+  VaultPaused,
+  VaultUnpaused,
+} from "../generated/VaultsModule/VaultsModule";
 import { Vault } from "../generated/schema";
 
 export function handleVaultAdded(event: VaultAdded): void {
   const vault = new Vault(event.params.id);
   vault.address = event.params.impl;
+  vault.minRate = event.params.minRate;
+  vault.maxRate = event.params.maxRate;
+  vault.paused = false;
+  vault.save();
+}
+
+export function handleMinRateChanged(event: MinRateChanged): void {
+  const vault = new Vault(event.params.id);
+  vault.minRate = event.params.newMinRate;
+  vault.save();
+}
+
+export function handleMaxRateChanged(event: MaxRateChanged): void {
+  const vault = new Vault(event.params.id);
+  vault.maxRate = event.params.newMaxRate;
+  vault.save();
+}
+
+export function handleVaultPaused(event: VaultPaused): void {
+  const vault = new Vault(event.params.id);
+  vault.paused = true;
+  vault.save();
+}
+
+export function handleVaultUnpaused(event: VaultUnpaused): void {
+  const vault = new Vault(event.params.id);
+  vault.paused = false;
   vault.save();
 }
